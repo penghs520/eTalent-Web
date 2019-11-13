842ef6321c5198a1b647dae9731fc7829c479551
luciarose123 <wangqs@qinjee.cn>
2019-11-13 18:43:09 +0800

    下班提交
    登陆页面

---
+++ src/views/login/components/swiper.vue
@@ -0,0 +1,24 @@
+
+<style scoped>
+</style>
+
+<template>
+    <div>
+        <el-carousel height="600px">
+            <el-carousel-item v-for="item in 4" :key="item">
+                <img :src="item" alt />
+            </el-carousel-item>
+        </el-carousel>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "login_wrap",
+    data() {
+        return {
+            
+        };
+    }
+};
+</script>

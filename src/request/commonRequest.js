9607bff463db0f30e42491431192fc226f17aaf3
qinjieheyang <57243958+qinjieheyang@users.noreply.github.com>
2019-12-12 16:35:59 +0800

    合并代码
    内置报表

--- src/request/commonRequest.js
+++ src/request/commonRequest.js
@@ -1,6 +1,6 @@
 // 请求公用数据法用法
 import base from '../assets/js/base';
-import {staff_api1,staff_api2} from './api';
+import {staff_api1,staff_api2, orgRepair_api1} from './api';
 
 // 请求方法
 let method = {
@@ -29,6 +29,23 @@ let method = {
             }
         })
     },
+
+    // 从彭洪思这里取机构树
+    getOrgTreeFromP(callback) {
+        let send = {
+            isEnable: 0
+        };
+        base.log("s", "commonRequest--机构树p", send);
+        orgRepair_api1(send, res => {
+            let d = res.data;
+            base.log("r", "commonRequest--机构树p", d);
+            if (d.success) {
+                callback(d.result.list);
+            } else {
+                base.error(d);
+            }
+        });
+    },
 };
 
 // 页面调用的方法
@@ -36,7 +53,12 @@ let commonRequest = {
     // 机构树
     getOrg(callback) {
         method.orgTree_getCompany(callback);
-    }
+    },
+
+    // 从彭洪思那里取机构树
+    getOrgP(callback) {
+        method.getOrgTreeFromP(callback);
+    },
 };
 
 export default commonRequest;
